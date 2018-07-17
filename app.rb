8c8539d11d3cbca7b0b73fc1aa8b0abda063cbd1
Azwilson <azya.w99@gmail.com>
2018-07-17 20:55:10 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -27,7 +27,7 @@ final_string
   get '/say/:word1/:word2/:word3/:word4/:word5' do 
  "#{params[:word1]} #{params[:word2]} #{params[:word3]} #{params[:word4]} #{params[:word5]}"
  end
- end 
+ 
  
  
  
