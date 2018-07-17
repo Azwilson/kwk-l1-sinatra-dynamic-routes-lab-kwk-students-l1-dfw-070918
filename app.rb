15d828fef7067ed8b4a7e56f108b790b4535db77
Azwilson <azya.w99@gmail.com>
2018-07-17 20:03:29 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -5,11 +5,20 @@ class App < Sinatra::Base
   get '/reversename/:name'
    reversename.name == params[:id]
   end.first
- erb :'/reversename/show.html'
+  erb :'/reversename/show.html'
  
   
   params = {
     :id =>1
+  }
+  
+  get '/square/:number'
+   square.number == params[:id]
+  end.second
+  erb : '/square/:number'
+  
+  params = {
+    :id =>2
   }
   
   
