347debc15e3aa1e2b69fadea6d180aef4a5b98d9
Azwilson <azya.w99@gmail.com>
2018-07-17 19:55:59 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,10 +1,15 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+ 
  get '/reversename/ :name'
+ reversename.name == params[:id]
+  end.first
+ erb :'/reversename/show.html'
  end
 
-
+ 
+ 
  get '/square/:number'
  end
 
